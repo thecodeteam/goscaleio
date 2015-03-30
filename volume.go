a32a2b47ab516e413e84b5bf3656c44fa0f7a540
Clinton Kitson <clintonskitson@gmail.com>
2015-03-30 12:51:09 -0700

    added volume local

--- volume.go
+++ volume.go
@@ -3,9 +3,21 @@ package goscaleio
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strings"
 
 	types "github.com/emccode/goscaleio/types/v1"
 )
+
+type SdcMappedVolume struct {
+	MdmID     string
+	VolumeID  string
+	SdcDevice string
+}
 
 type Volume struct {
 	Volume *types.Volume
@@ -71,4 +83,54 @@ func (storagePool *StoragePool) FindVolume(id, name, href string) (volume *types
 	//
 	// return &types.Volume{}, errors.New("Couldn't find volumes")
 	return &types.Volume{}, nil
+}
+
+func GetLocalVolumeMap() (mappedVolumes []*SdcMappedVolume, err error) {
+
+	// get sdc kernel guid
+	// /bin/emc/scaleio/drv_cfg --query_guid
+	// sdcKernelGuid := "271bad82-08ee-44f2-a2b1-7e2787c27be1"
+
+	mappedVolumesMap := make(map[string]*SdcMappedVolume)
+
+	out, err := exec.Command("/bin/emc/scaleio/drv_cfg", "--query_vols").Output()
+	if err != nil {
+		return []*SdcMappedVolume{}, fmt.Errorf("Error querying volumes: ", err)
+	}
+
+	result := string(out)
+	lines := strings.Split(result, "\n")
+
+	for _, line := range lines {
+		split := strings.Split(line, " ")
+		if split[0] == "VOL-ID" {
+			mappedVolume := &SdcMappedVolume{MdmID: split[3], VolumeID: split[1]}
+			mdmVolumeID := fmt.Sprintf("%s-%s", mappedVolume.MdmID, mappedVolume.VolumeID)
+			mappedVolumesMap[mdmVolumeID] = mappedVolume
+		}
+	}
+
+	diskIDPath := "/dev/disk/by-id"
+	files, _ := ioutil.ReadDir(diskIDPath)
+	r, _ := regexp.Compile(`^emc-vol-\w*-\w*$`)
+	for _, f := range files {
+		matched := r.MatchString(f.Name())
+		if matched {
+			mdmVolumeID := strings.Replace(f.Name(), "emc-vol-", "", 1)
+			devPath, _ := filepath.EvalSymlinks(fmt.Sprintf("%s/%s", diskIDPath, f.Name()))
+			mappedVolumesMap[mdmVolumeID].SdcDevice = devPath
+		}
+	}
+
+	keys := make([]string, 0, len(mappedVolumesMap))
+	for key := range mappedVolumesMap {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+
+	for _, key := range keys {
+		mappedVolumes = append(mappedVolumes, mappedVolumesMap[key])
+	}
+
+	return mappedVolumes, nil
 }
